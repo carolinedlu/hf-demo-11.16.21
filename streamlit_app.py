a1a4d9ae879b2be4ebeafc2e781f2106245c004b
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-11-16 15:24:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,10 @@ def read_file(filename):
 path_to_file = st.secrets.BUCKET_NAME + "/HelloFreshExample - SampleData.csv"
 st.write(path_to_file)
 
-content = read_file(path_to_file)
+# content = read_file(path_to_file)
 
-# Print results.
-st.subheader("Read data from CSV file:")
-for line in content.strip().split("\n")[1:]:
-    first_name, last_name, dietary_restrictions, fave_ingredient = line.split(",")
-    st.write(f"{first_name} {last_name}'s favorite ingredient is {fave_ingredient}.")
+# # Print results.
+# st.subheader("Read data from CSV file:")
+# for line in content.strip().split("\n")[1:]:
+#     first_name, last_name, dietary_restrictions, fave_ingredient = line.split(",")
+#     st.write(f"{first_name} {last_name}'s favorite ingredient is {fave_ingredient}.")
